e84f6f8b127521b34690265e6fd8c8250dea81c2
Iraida <ira.serq.1996@gmail.com>
2023-07-23 10:39:25 +0300

    исправила ошибки

--- src/js/modules/smooth-scroll.js
+++ src/js/modules/smooth-scroll.js
@@ -1,22 +1,22 @@
 export function smoothScroll() {
-    const menuLink = document.querySelectorAll('.menu__link[data-goto]');
+    const menuLink = document.querySelectorAll('.menu-link[data-goto]');
     if (menuLink.length > 0){
-        menuLink.forEach(menuLink => {
-            menuLink.addEventListener('click', onMenuLinkClick);
-        })
-
         function onMenuLinkClick(event) {
             const menuLink = event.target;
             if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
                 const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageXOffset - document.querySelector('header').offsetHeight;
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
                 window.scrollTo({
                     top: gotoBlockValue,
                     behavior: "smooth"
                 });
-                event.preventDerault();
+                event.preventDefault();
             }
         }
+
+        menuLink.forEach(menuLink => {
+            menuLink.addEventListener('click', onMenuLinkClick);
+        })
     }
 }
